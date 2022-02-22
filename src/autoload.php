a7e225928e72876d6bac57e5ec7b329ff526b7ae
Valentin Startsev <bigbagperm@gmail.com>
2022-02-22 18:41:18 +0500

    autoload change

--- src/autoload.php
+++ src/autoload.php
@@ -5,11 +5,13 @@ define('MIXPLAT_CLIENT_PATH', dirname(__FILE__));
 function mixplatLoadClass($className)
 {
     $basedClass = str_replace('MixplatClient\\', '', $className);
+    $basedClass = str_replace('\\', '/', $basedClass);
     $path = MIXPLAT_CLIENT_PATH . DIRECTORY_SEPARATOR . $basedClass . '.php';
 
     if (file_exists($path)) {
         require $path;
     }
+
 }
 
 spl_autoload_register('mixplatLoadClass');
